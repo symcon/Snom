436b10b0b75937c6a7a1258cec54ff3b8f655fc4
simon golpe <simongolpe@gmail.com>
2023-09-22 17:39:11 +0200

    add webhook and send xml method

--- SnomHelloWorld/module.php
+++ SnomHelloWorld/module.php
@@ -1,20 +1,22 @@
 <?php
 
-class SnomHelloWorld extends IPSModule {
+class SnomHelloWorld extends IPSModuleStrict {
     // Überschreibt die interne IPS_Create($id) Funktion
-    public function Create() {
+    public function Create(): void {
         // Diese Zeile nicht löschen.
         parent::Create();
-
+        $this->RegisterVariableString("LocalIp", "Local IP address");
         $this->RegisterVariableString("SymconCurrentVersion", "Symcon Current version");
         $this->RegisterVariableString("SnomXml", "Snom XML");
+        $this->RegisterHook("snom/" . $this->InstanceID);
     }
     // Überschreibt die intere IPS_ApplyChanges($id) Funktion
-    public function ApplyChanges() {
+    public function ApplyChanges(): void {
         // Diese Zeile nicht löschen
         parent::ApplyChanges();
+        $this->SetValue("LocalIp", "127.0.0.1");
         $this->SetValue("SymconCurrentVersion", IPS_GetKernelVersion());
-        $this->SetValue("SnomXml", $this->SnomIPPhoneText("Hello World", 10000));
+        $this->SetValue("SnomXml", $this->SnomIPPhoneText("Hello you", 10000));
     }
     /**
     * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
@@ -23,17 +25,16 @@ class SnomHelloWorld extends IPSModule {
     * SNM_SendText();
     *
     */
-    // public function SendXml() {
-    //     $PhoneIPAddress = "192.168.178.69";
-    //     $LocalIPAddress = Sys_GetNetworkInfo()[0]['IP'];
-    //     $HookURL = sprintf("http://%s/snom/%d", $LocalIPAddress, $this->InstanceID);
-    //     $URL = sprintf("http://%s/minibrowser?url=%s", $PhoneIPAddress, $HookURL);
 
-    //     $this->SnomIPPhoneText("Hello world", 10000);
-    //     file_get_contents($URL);
-    // }
+    /**
+    * This function will be called by the hook control. Visibility should be protected!
+    */
+    protected function ProcessHookData(): void {
+        header("Content-Type: text/xml");
+        echo $this->GetValue("SnomXml");
+    }
 
-    private function SnomIPPhoneText(string $text, int $timeout) {
+    private function SnomIPPhoneText(string $text, int $timeout): string {
         header("Content-Type: text/xml");
         $xml = new DOMDocument('1.0', 'UTF-8');
         $xml->formatOutput = true;
@@ -47,5 +48,13 @@ class SnomHelloWorld extends IPSModule {
         $xmlRoot->appendChild($fetch);
 
         return $xml->saveXML();
+    }
+    public function SendXml(): void {
+        $PhoneIPAddress = "192.168.8.75";
+        $HookURL = sprintf("http://%s:3777/hook/snom/%d", $this->GetValue("LocalIp"), $this->InstanceID);
+        $URL = sprintf("http://%s/minibrowser.htm?url=%s", $PhoneIPAddress, $HookURL);
+
+        // $this->SnomIPPhoneText("Hello world", 10000);
+        file_get_contents($URL);
     }
 }
