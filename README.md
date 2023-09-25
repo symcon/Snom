6a1faa4a9325c62c739c6949fb4856b19d214149
simon golpe <simongolpe@gmail.com>
2023-09-25 10:17:50 +0200

    ad form.json

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Librarary for integrating [Snom IP phones](https://www.snom.com/en/products/) in the [Symcon](https://www.symcon.de/en/product/) building automation system.
+
+Snom URL examples:
+ - call forwarding:  
+http://192.168.8.75/dummy.htm?settings=save&fwd_all_enabled1=on&fwd_all_target1=468865
--- SnomHelloWorld/module.php
+++ SnomHelloWorld/module.php
@@ -5,36 +5,31 @@ class SnomHelloWorld extends IPSModuleStrict {
     public function Create(): void {
         // Diese Zeile nicht löschen.
         parent::Create();
-        $this->RegisterVariableString("LocalIp", "Local IP address");
-        $this->RegisterVariableString("SymconCurrentVersion", "Symcon Current version");
-        $this->RegisterVariableString("SnomXml", "Snom XML");
+        $this->RegisterPropertyString("PhoneIP", "");
+        $this->RegisterPropertyString("LocalIP", "");
+        $this->RegisterVariableString("MbRequestUrl", "Minibrowser request URL");
+        $this->RegisterVariableString("MbPage", "Minibrowser page");
         $this->RegisterHook("snom/" . $this->InstanceID);
+        $this->RegisterVariableString("SymconCurrentVersion", "Symcon Current version");
     }
     // Überschreibt die intere IPS_ApplyChanges($id) Funktion
     public function ApplyChanges(): void {
         // Diese Zeile nicht löschen
         parent::ApplyChanges();
-        $this->SetValue("LocalIp", "127.0.0.1");
         $this->SetValue("SymconCurrentVersion", IPS_GetKernelVersion());
-        $this->SetValue("SnomXml", $this->SnomIPPhoneText("Hello you", 10000));
+        $this->SetValue("MbPage", $this->GetIPPhoneTextItem("Hello you", 10000));
+        $this->SetValue("MbRequestUrl", $this->GetMbRequestUrl());
     }
-    /**
-    * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
-    * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
-    *
-    * SNM_SendText();
-    *
-    */
 
     /**
     * This function will be called by the hook control. Visibility should be protected!
     */
     protected function ProcessHookData(): void {
         header("Content-Type: text/xml");
-        echo $this->GetValue("SnomXml");
+        echo $this->GetValue("MbPage");
     }
 
-    private function SnomIPPhoneText(string $text, int $timeout): string {
+    private function GetIPPhoneTextItem(string $text, int $timeout): string {
         header("Content-Type: text/xml");
         $xml = new DOMDocument('1.0', 'UTF-8');
         $xml->formatOutput = true;
@@ -49,12 +44,23 @@ class SnomHelloWorld extends IPSModuleStrict {
 
         return $xml->saveXML();
     }
-    public function SendXml(): void {
-        $PhoneIPAddress = "192.168.8.75";
-        $HookURL = sprintf("http://%s:3777/hook/snom/%d", $this->GetValue("LocalIp"), $this->InstanceID);
-        $URL = sprintf("http://%s/minibrowser.htm?url=%s", $PhoneIPAddress, $HookURL);
 
-        // $this->SnomIPPhoneText("Hello world", 10000);
-        file_get_contents($URL);
+    private function GetMbRequestUrl(): string {
+        $HookURL = sprintf("http://%s:3777/hook/snom/%d", $this->ReadPropertyString("LocalIP"), $this->InstanceID);
+        $URL = sprintf("http://%s/minibrowser.htm?url=%s", $this->ReadPropertyString("PhoneIP"), $HookURL);
+
+        return $URL;
+    }
+
+       /**
+    * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+    * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+    *
+    * SNM_GetMbPage();
+    *
+    */
+
+    public function GetMbPage(): void {
+        file_get_contents($this->GetValue("MbRequestUrl"));
     }
 }
