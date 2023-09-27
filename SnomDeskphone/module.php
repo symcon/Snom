f6d1ac3cfc64d062040e1022c59e74590945f9aa
simon golpe <simongolpe@gmail.com>
2023-09-27 10:25:39 +0200

    add Snom deskphone module

---
+++ SnomDeskphone/module.php
@@ -0,0 +1,22 @@
+<?php
+
+class SnomDeskphone extends IPSModuleStrict {
+    public function Create(): void {
+        parent::Create();
+    }
+
+    public function ApplyChanges(): void {
+        parent::ApplyChanges();
+    }
+    
+    /**
+    * This function will be called by the hook control. Visibility should be protected!
+    */
+    // protected function ProcessHookData(): void {
+    //     header("Content-Type: text/xml");
+    // }
+
+
+    // Usage of public functions (prefix defined in module.json):
+    // SNM_D_GetPageContent();
+}
