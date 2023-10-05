360d2a72787e6de9f5c1188e3b63a1ea07735d96
simon golpe <simongolpe@gmail.com>
2023-10-05 15:59:08 +0200

    Add checkbox for setting fkey as only receive

--- SnomDeskphone/module.php
+++ SnomDeskphone/module.php
@@ -7,23 +7,35 @@ class SnomDeskphone extends IPSModuleStrict {
         $this->RegisterPropertyString("PhoneMac", "000413");
         $this->RegisterPropertyString("PhoneModel", "snomD785");
         $this->RegisterPropertyString("LocalIP", "127.0.0.1");
-        $this->RegisterPropertyBoolean("FunctionKeys", false);
         $this->RegisterFkeysProperties();
     }
 
     private function RegisterFkeysProperties(): void {
-        $fKeyIndexes = range(0, 1);
-        foreach ($fKeyIndexes as $fkeyIndex) {
-            $this->RegisterFkeyProperties($fkeyIndex);
-        }
+        $this->RegisterPropertyInteger("FkeyNo", -1);
+        $this->RegisterPropertyInteger("ActionVariableId", -1);
+        $this->RegisterPropertyBoolean("RecieveOnly", false);
+        $this->RegisterPropertyInteger("ActionValue", -1);
+        $this->RegisterPropertyString("ActionHook", "/snom/myVarID");
+        $this->RegisterPropertyString("FkeyLabel", "my label");
+        $this->RegisterPropertyString("FkeyColorOn", "red");
+        $this->RegisterPropertyString("FkeyColorOff", "green");
+        $this->SendDebug('INFO', print_r('Fkey properties registered', true), 0);
     }
-    private function RegisterFkeyProperties(int $fkeyIndex): void {
-        $this->RegisterPropertyString(sprintf("ActionURLfKey%d", $fkeyIndex), "");
-        $this->RegisterPropertyString(sprintf("LabelFkey%d", $fkeyIndex), "");
-        $this->RegisterPropertyInteger(sprintf("LedfKey%d", $fkeyIndex), 0);
-        $this->RegisterPropertyString(sprintf("ColorFkey%dOn", $fkeyIndex), "green");
-        $this->RegisterPropertyString(sprintf("ColorFkey%dOff", $fkeyIndex), "red");
-    }
+
+    // private function RegisterFkeysProperties(): void {
+    //     $fKeyIndexes = range(0, 1);
+    //     foreach ($fKeyIndexes as $fkeyIndex) {
+    //         $this->RegisterFkeyProperties($fkeyIndex);
+    //     }
+    // }
+    
+    // private function RegisterFkeyProperties(int $fkeyIndex): void {
+    //     $this->RegisterPropertyString(sprintf("ActionURLfKey%d", $fkeyIndex), "");
+    //     $this->RegisterPropertyString(sprintf("LabelFkey%d", $fkeyIndex), "");
+    //     $this->RegisterPropertyInteger(sprintf("LedfKey%d", $fkeyIndex), 0);
+    //     $this->RegisterPropertyString(sprintf("ColorFkey%dOn", $fkeyIndex), "green");
+    //     $this->RegisterPropertyString(sprintf("ColorFkey%dOff", $fkeyIndex), "red");
+    // }
 
     public function ApplyChanges(): void {
         parent::ApplyChanges();
@@ -41,14 +53,48 @@ class SnomDeskphone extends IPSModuleStrict {
         return sprintf("Phone with IP %s is not reachable", $phoneIp);
     }
 
-    public function SetFkeySettings(int $fKeyIndex): void {
-        $actionUrl = sprintf("ActionURLfKey%d", $fKeyIndex);
-        $actionUrlValue = $this->ReadPropertyString($actionUrl);
-        $actionUrlValue ? $fkeyType="url" : $fkeyType = "none";
+    public function SetValueFieldVisibility(bool $RecieveOnly): void {
+        $this->UpdateFormField("ActionValue", "visible", !$RecieveOnly);
+    }
 
-        $label = sprintf("LabelFkey%d", $fKeyIndex);
-        $labelValue = urlencode($this->ReadPropertyString($label));
-        $fkeyValue = urlencode($fkeyType ." " . $actionUrlValue);
+    public function SetVariableId(int $variableId): void {
+        if ($this->ReadPropertyBoolean("RecieveOnly")) {
+            $this->SendDebug('SET', print_r('Recieve only fkey', true), 0);
+        }
+        else {
+            $this->UpdateFormField("ActionValue", "variableID", $variableId);
+            $this->SendDebug('SET', print_r('set variable id', true), 0);     
+        }
+    }
+
+    public function CreateHook(int $variableId): void {
+        $this->RegisterHook("snom/" . $variableId);
+        $this->UpdateFormField("ActionHook", "value", sprintf("/snom/%d", $variableId));
+        $this->SendDebug('create', print_r('hook created' . $this->ReadPropertyString("ActionHook"), true), 0);
+    }
+
+    public function SetFkeySettings(int $fKey, bool $isRecieveOnly, string $variableHook, string $labelValue): void {
+        $fKeyIndex = $fKey-1;
+        $this->SendDebug("INFO", print_r("Configuring fkey " . $fKeyIndex, true), 0);
+        // $actionUrl = sprintf("ActionURLfKey%d", $fKeyIndex);
+        // $actionUrlValue = $this->ReadPropertyString($actionUrl);
+        // $isRecieveOnly = $this->ReadPropertyBoolean("RecieveOnly");
+        // $isRecieveOnly ? $fkeyType="none" : $fkeyType = "url";
+        // $actionUrlValue ? $fkeyType="url" : $fkeyType = "none";
+        // $variableHook = $this->ReadPropertyString("ActionHook");
+
+        // $labelValue = urlencode($this->ReadPropertyString("FkeyLabel"));
+
+        if ($isRecieveOnly) {
+            $fkeyType="none";
+            $fkeyValue = urlencode($fkeyType);
+        }
+        else {
+            $fkeyType = "url";
+            $localIp = $this->ReadPropertyString("LocalIP");
+            $fkeyValue = urlencode($fkeyType ." " . $localIp . ":3777/hook" . $variableHook);
+        }
+
         $urlQuery = sprintf("settings=save&fkey%d=%s&fkey_label%d=%s", $fKeyIndex, $fkeyValue, $fKeyIndex, $labelValue);
 
         if ($this->ReadPropertyString("PhoneModel")=="snomD735") {
