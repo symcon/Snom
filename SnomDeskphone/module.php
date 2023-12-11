a8c4fdb5229b62a01be5017ac26d17e9ea524c86
simon golpe <simongolpe@gmail.com>
2023-12-11 17:50:57 +0100

    refactor

--- SnomDeskphone/module.php
+++ SnomDeskphone/module.php
@@ -19,6 +19,8 @@ class SnomDeskphone extends IPSModuleStrict
     {
         parent::ApplyChanges();
         $this->SetSummary($this->ReadPropertyString('PhoneIP'));
+        $this->SetValue('PhoneModel', "");
+        $this->SetValue('PhoneMac', "");
     }
 
     public function MessageSink(int $TimeStamp, int $SenderID, int $Message, array $Data): void
